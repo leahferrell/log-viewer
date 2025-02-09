526f58913d47ca7898bbb648a0cc92ee08bad817
Leah Ferrell <leahferrell@Leahs-MBP.home>
2025-02-09 11:36:45 -0700

    setting up front end scaffold

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,24 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import { Provider } from 'react-redux'
+import { createRoot } from 'react-dom/client'
+import { HashRouter as Router } from 'react-router'
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+import { store } from './stores/store'
+import App from './App'
+
+const container = document.getElementById('root')
+
+if (container) {
+  const root = createRoot(container)
+
+  root.render(
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>,
+  )
+} else {
+  throw new Error(
+    "Root element with ID 'root' was not found in the document. Ensure there is a corresponding HTML element with the ID 'root' in your HTML file.",
+  )
+}

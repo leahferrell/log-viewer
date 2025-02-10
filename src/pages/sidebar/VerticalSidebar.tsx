d4bd3e827e43c0f81b7695c3112329047e166db9
leahferrell <leahferrell03@gmail.com>
2025-02-09 19:37:54 -0700

    fixing reference

--- src/pages/sidebar/VerticalSidebar.tsx
+++ src/pages/sidebar/VerticalSidebar.tsx
@@ -1,17 +1,29 @@
-import {Code2, DatabaseIcon, FolderHeart, HelpCircleIcon, HistoryIcon, LucideIcon} from 'lucide-react'
+import {
+  Code2,
+  DatabaseIcon,
+  FolderHeart,
+  HelpCircleIcon,
+  HistoryIcon,
+  LucideIcon,
+} from 'lucide-react'
 import clsx from 'clsx'
 
-import {Tooltip, TooltipContent, TooltipProvider, TooltipTrigger} from '../../components/ui/tooltip.tsx'
-import {Button} from '../../components/ui/button.tsx'
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '../../components/ui/tooltip.tsx'
+import { Button } from '../../components/ui/button.tsx'
 import ToggleColorMode from '../../components/ToggleColorMode.tsx'
 import AppSettingDialog from '../settings/AppSettingsDialog.tsx'
-import {useState} from 'react'
+import { useState } from 'react'
 
 interface SideButtonProps {
-  id: string;
-  icon: LucideIcon;
-  label: string;
-  onClick?: () => void;
+  id: string
+  icon: LucideIcon
+  label: string
+  onClick?: () => void
 }
 
 const SideButton = ({ id, icon: Comp, label, onClick }: SideButtonProps) => {
@@ -26,8 +38,8 @@ const SideButton = ({ id, icon: Comp, label, onClick }: SideButtonProps) => {
       <Tooltip>
         <TooltipTrigger asChild>
           <Button
-            variant="ghost"
-            size="icon"
+            variant='ghost'
+            size='icon'
             className={clsx(
               'size-8 rounded-lg',
               activeSide == id ? 'bg-muted' : '',
@@ -35,10 +47,13 @@ const SideButton = ({ id, icon: Comp, label, onClick }: SideButtonProps) => {
             aria-label={label}
             onClick={onClick ?? handleClick}
           >
-            <Comp className="size-4" />
+            <Comp className='size-4' />
           </Button>
         </TooltipTrigger>
-        <TooltipContent side="right" sideOffset={5}>
+        <TooltipContent
+          side='right'
+          sideOffset={5}
+        >
           {label}
         </TooltipContent>
       </Tooltip>
@@ -49,19 +64,35 @@ const SideButton = ({ id, icon: Comp, label, onClick }: SideButtonProps) => {
 export function VerticalSidebar() {
   return (
     <div>
-      <nav className="grid gap-0.5 p-0.5 [&>*]:place-self-center">
-        <SideButton id="database" label="Database" icon={DatabaseIcon} />
-        <SideButton id="favorite" label="Favorite" icon={FolderHeart} />
-        <SideButton id="code" label="Code" icon={Code2} />
-        <SideButton id="history" label="History" icon={HistoryIcon} />
-      </nav>
-      <nav className="mt-auto grid gap-1 p-0 [&>*]:place-self-center">
+      <nav className='grid gap-0.5 p-0.5 [&>*]:place-self-center'>
         <SideButton
-          id="help"
-          label="Help"
+          id='database'
+          label='Database'
+          icon={DatabaseIcon}
+        />
+        <SideButton
+          id='favorite'
+          label='Favorite'
+          icon={FolderHeart}
+        />
+        <SideButton
+          id='code'
+          label='Code'
+          icon={Code2}
+        />
+        <SideButton
+          id='history'
+          label='History'
+          icon={HistoryIcon}
+        />
+      </nav>
+      <nav className='mt-auto grid gap-1 p-0 [&>*]:place-self-center'>
+        <SideButton
+          id='help'
+          label='Help'
           icon={HelpCircleIcon}
           onClick={() => {
-            open('https://github.com/l1xnan/duckling');
+            open('https://github.com/leahferrell/log-viewer')
           }}
         />
         <ToggleColorMode />
